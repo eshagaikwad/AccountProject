2f95f7edbbc7eaa1368b0ae74367e3a4cd8f3997
ESHA MILIND GAIKWAD <Esha.GAIKWAD@asl.mum.sst>
2024-07-30 20:05:52 +0530

    Add project files.

---
+++ AccountProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Collections;
+using AccountLibrary;
+using AccountLibrary.Model;
+
+
+namespace AccountProjectWithList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            AccountManager.account = SerialDesrial.DeserializeData();
+
+            AccountManager.UserSelection();
+
+            SerialDesrial.SerializeData(AccountManager.account);
+
+        }
+
+    }
+
+}
